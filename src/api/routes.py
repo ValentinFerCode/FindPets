ee2a8948339c9413f2afd4f6fca18acff29fdcd9
xControlFreak <xsimplemail@gmail.com>
2023-02-03 18:08:52 +0000

    Funcion iniciar sesion

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,56 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# @api.route('/hello', methods=['POST', 'GET'])
+# def handle_hello():
+
+#     response_body = {
+#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+#     }
+
+#     return jsonify(response_body), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    # Obtengo credenciales del usuario desde el cuerpo de la solicitud
+    username = request.json.get("username")
+    password = request.json.get("password")
+
+    usuario_login = Usuario.query.filter_by(username=username).first()
+
+    if username != username_login.username | password != usuario_login.password:
+        return jsonify({"message": "Usuario o contraseña incorrectos"}), 401
+
+    else: 
+        return jsonify({"message": "Inicio de sesión exitoso"})
+
+    #Token de acceso
+    access_token = create_access_token(identity=user.id)
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "access_token": access_token,
     }
-
-    return jsonify(response_body), 200
