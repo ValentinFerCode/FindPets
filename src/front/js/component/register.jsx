bee7967c177fcdb8ce0c0e8b66d42d4305b135e2
A0X0E0L <ax13xxi@gmail.com>
2023-02-06 13:54:00 +0000

    Estructura del formulario completa

--- src/front/js/component/register.jsx
+++ src/front/js/component/register.jsx
@@ -3,33 +3,52 @@ import React from "react";
 export const Register = () => {
 
     return (
-        <div classNameName="container col-25 p-12">
-            <div className=" d-flex align-items-center">
-                <form>
-                    <div className="col col-3">
-                        <label for="exampleInputEmail1" className="form-label">Email</label>
+        <div className="mx-auto w-50 card text-center m-4">
+
+            <form>
+                <div className="col col-3 d-flex mx-auto">
+                    <div className>
+                        <h7>
+                            <b>Nombre</b>
+                        </h7>
+                        {/* <label for="exampleInputEmail1" className="form-label">Email</label> */}
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    </div>
+                </div>
+                <div className>
+                    <h7>
+                        <b>Apellido</b>
+                    </h7>
+                    <div className="col col-3 d-flex mx-auto">
+                        {/* <label for="exampleInputPassword1" className="form-label">Password</label> */}
+                        <input type="text" className="form-control" id="exampleInputPassword1"/></div>
+                </div>
+
+                <div className="col col-3 d-flex mx-auto">
+                    <div className>
+                        <h7>
+                            <b>Correo Electrónico</b>
+                        </h7>
+                        {/* <label for="exampleInputEmail1" className="form-label">Email</label> */}
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                     </div>
-                    <div className="col col-3">
-                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                </div>
+                <div className>
+                    <h7>
+                        <b>Nombre de Usuario</b>
+                    </h7>
+                    <div className="col col-3 d-flex mx-auto">
+                        {/* <label for="exampleInputPassword1" className="form-label">Password</label> */}
+                        <input type="Text" className="form-control" id="exampleInputPassword1"/></div>
+                    <h7>
+                        <strong>Contraseña</strong>
+                    </h7>
+                    <div className="col col-3 d-flex mx-auto">
+                        {/* <label for="exampleInputPassword1" className="form-label">Password</label> */}
                         <input type="password" className="form-control" id="exampleInputPassword1"/></div>
-                    <div className="col col-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-
-                    <div className="col col-3">
-                        <label for="exampleInputEmail1" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    </div>
-                    <div className="col col-3">
-                        <label for="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1"/></div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-
-
-            </div>
+                    <button type="submit" className="  d-flex mx-auto btn btn-warning ">Registrarse</button>
+                </div>
+            </form>
 
 
         </div>
