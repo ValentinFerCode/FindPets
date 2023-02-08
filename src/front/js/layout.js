0e14c7b4fea1bc90a88a7b664a5223a7f841c2c0
A0X0E0L <ax13xxi@gmail.com>
2023-02-08 13:09:16 +0000

    the mother fucker card

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -14,6 +14,7 @@ import {Footer} from "./component/footer.jsx";
 import {PetForm} from "./pages/formencontrado.jsx";
 // import {FormAdoptar} from "./pages/formadoptar.jsx";
 import {Register} from "./component/register.jsx";
+import {FormAdoptar} from "./pages/formadoptar.jsx";
 
 // create your first component
 const Layout = () => {
@@ -37,6 +38,8 @@ const Layout = () => {
                             path="/single/:theid"/> {/* <Route element={<Recuperacion_clave />} path="/recuperacion_clave"/> */}
                         <Route element={<PetForm/>}
                             path="/formencontrado"/> {/* <Route element={<FormAdoptar/>} path="/formadoptar"/> */}
+                        <Route element={<FormAdoptar/>}
+                            path="/formadoptar"/>
                         <Route element={
                             <h1>Not found!</h1>
                         }/>
--- src/front/js/pages/formadoptar.jsx
+++ src/front/js/pages/formadoptar.jsx
@@ -3,78 +3,68 @@ import React from "react";
 
 export const FormAdoptar = () => {
 
-    return (<div className="mx-auto w-50 card text-center m-4  rounded-5   border border-success p-2 mb-2   border border-5"
-        style={
-            {background: "#B9F3FC"}
-    }>
-        <form>
+    return (
+        <>
+            <div className="d-flex w-100 justify-content-center ">
+                {/* encerrar todo este card dentro de una etiqueta Link */}
 
+                <div className="mx-auto w-50 card text-center m-4  rounded-5   border border-success p-2 mb-2   border border-5"
+                    style={
+                        {background: "#B9F3FC"}
+                }>
+                    <div className="row g-0">
+                        <p>
+                            <div className="col-md-4">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHzfL9ih32nOG1UrP1LPIifbNHatB0P-GUIg&usqp=CAU" className="img-fluid rounded-start" alt="..."/>
+                            </div>
+                        </p>
+                        <div className="col-md-8">
+                            <p></p>
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    Scooby-Doo
+                                </h5>
+                                <p className="card-text colm">
+                                    {/* hcer filas aquI */} </p>
+                                <p className="card-text">
+                                    <small className="text">
+                                        <div>
+                                            <strong>
+                                                <p>
+                                                    Genero:</p>
 
-            <div className="col col-3 d-flex mx-auto">
-                <div className>
-                    <h7>
-                        <b>Genero</b>
-                    </h7>
+                                                <p>Tamaño:
+                                                </p>
+                                                <p>
+                                                    Color:</p>
+                                                <p>
+                                                    Nombre:</p>
+                                                <p>
+                                                    Edad:</p>
+                                                <p>
+                                                    Raza :</p>
+                                                <p>
+                                                    Estado:</p>
+                                                <p>
+                                                    Especie:</p>
 
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                            </strong>
+                                            <p>
+                                                <button type="button" class="btn btn-info">Adoptar</button>
+                                            </p>
+                                        </div>
+                                    </small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className>
-                <h7>
-                    <b>Nombre</b>
-                </h7>
-                <div className="col col-3 d-flex mx-auto">
-
-                    <input type="text" className="form-control" id="exampleInputPassword1"/></div>
-            </div>
-
-            <div className="col col-3 d-flex mx-auto">
-                <div className>
-                    <h7>
-                        <b>Tamaño</b>
-                    </h7>
-
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                </div>
-            </div>
-            <div className>
-                <h7>
-                    <b>Color</b>
-                </h7>
-                <div className="col col-3 d-flex mx-auto">
-
-                    <input type="Text" className="form-control" id="exampleInputPassword1"/></div>
-                <h7>
-                    <strong>Edad</strong>
-                </h7>
-                <div className="col col-3 d-flex mx-auto">
-
-                    <input type="password" className="form-control" id="exampleInputPassword1"/></div>
 
             </div>
-            <h7>
-                <strong>Raza</strong>
-            </h7>
-            <div className="col col-3 d-flex mx-auto">
-
-                <input type="password" className="form-control" id="exampleInputPassword1"/></div>
-            <h7>
-                <strong>Estado</strong>
-            </h7>
-            <div className="col col-3 d-flex mx-auto">
-
-                <input type="password" className="form-control" id="exampleInputPassword1"/></div>
-
-            <strong>Especie</strong>
-
-            <div className="col col-3 d-flex mx-auto">
-
-                <input type="password" className="form-control" id="exampleInputPassword1"/></div>
-            <button type="submit" className="  d-flex mx-auto btn btn-warning ">Enviar</button>
 
 
-        </form>
-    </div>);
+        </>
+    );
 };
 
 
