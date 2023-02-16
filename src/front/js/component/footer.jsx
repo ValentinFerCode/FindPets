48263470047e15f49c4541a26694df8b287d017d
A0X0E0L <ax13xxi@gmail.com>
2023-02-16 20:16:36 +0000

    fusion de santiago

--- src/front/js/component/footer.jsx
+++ src/front/js/component/footer.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+// import fontawesome from "react";
 
 export const Footer = () => (
-  <footer className="footer mt-auto py-3 text-center bg-danger">
-    <p>
-      Made with <i className="fa fa-heart text-danger" /> by{" "}
-      <a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-    </p>
+  <footer className="footer">
+    <ul className="">
+      <p>@ 2023 - Todos los derechos reservados.</p>
+
+      <p>Teléfono (+598) 2603 8832 | consultas@findpets.com</p>
+      <p>
+        <link rel="stylesheet" href="https://es.wikipedia.org/wiki/WhatsApp" />
+      </p>
+
+      <a href="/">
+        <span class="navbar-brand mb-0 h1 text-white fs-2">
+          <i class="fa-brands fa-square-facebook"></i>
+        </span>
+      </a>
+    </ul>
   </footer>
 );
