bef4122e3dff4a83ed983d8b5118c36b2714b094
A0X0E0L <ax13xxi@gmail.com>
2023-02-23 14:32:41 +0000

    Arreglos insignificantes

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,123 +1,111 @@
-import React, { useState, useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Context } from "../store/appContext.js";
-import { useNavigate, Navigate } from "react-router-dom";
+import React, {useState, useContext, useEffect} from "react";
+import {Link} from "react-router-dom";
+import {Context} from "../store/appContext.js";
+import {useNavigate, Navigate} from "react-router-dom";
 
 export const Navbar = () => {
-  const { store, actions } = useContext(Context);
-  const navigate = useNavigate();
+    const {store, actions} = useContext(Context);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    actions.getOneUser(store.userSession.id);
-  }, []);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        actions.getOneUser(store.userSession.id);
+    }, []);
 
-  function handleLogout() {
-    actions.logout(); //cerrar la sesion
-    navigate("/"); //usamos navigate para redireccionar
-  }
+    function handleLogout() {
+        actions.logout(); // cerrar la sesion
+        navigate("/"); // usamos navigate para redireccionar
+    }
 
-  return (
-    <nav className="navbar navbar-expand-lg ">
-      <div className="container-fluid">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          {/* <span className="navbar-brand mb-0 h1 text-white fs-2"> */}
-          <img src="https://i.imgur.com/tkKWUot.png" className="logo-pagina" />
-          {/* FindPets
-            <i className="fa fa-paw mx-1"></i>
-          </span> */}
-        </Link>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item fs-6">
-              <Link to="/listas" style={{ textDecoration: "none" }}>
-                <span className="text-white m-2">Lista de mascotas</span>
-              </Link>
-            </li>
-          </ul>
-          <form className="d-flex">
-            {store.auth === false ? (
-              <Link to="/login">
-                <button
-                  type="button"
-                  className="btn btn-outline-light mx-3 rounded-pill btn-lg"
-                >
-                  <i className="fa fa-sign-in-alt mx-2"></i>
-                  Iniciar sesión
-                </button>
-              </Link>
-            ) : null}
-            {store.auth === false ? (
-              <Link to="/register">
-                <button
-                  type="button"
-                  className="btn btn-light rounded-pill btn-lg"
-                >
-                  <i className="fa fa-user-plus mx-2"></i>
-                  Crear cuenta
-                </button>
-              </Link>
-            ) : null}
-            {store.auth === true ? (
-              <div className="dropdown d-flex float-end">
-                <button
-                  className="btn btn-light rounded-pill btn-lg dropdown-toggle"
-                  type="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <i className="fa fa-plus-circle mx-2"></i>
-                  Publicar
-                </button>
-                <ul className="dropdown-menu">
-                  <Link to={"/formencontrado"}>
-                    <button className="dropdown-item float-start">
-                      Mascota perdida
-                    </button>
-                  </Link>
-                  <Link to={"/formadoptar"}>
-                    <button className="dropdown-item float-start">
-                      Mascota para adoptar
-                    </button>
-                  </Link>
-                </ul>
-              </div>
-            ) : null}
+    return (
+        <nav className="navbar navbar-expand-lg ">
+            <div className="container-fluid">
+                <Link to="/"
+                    style={
+                        {textDecoration: "none"}
+                }>
 
-            {store.auth === true ? (
-              <div className="dropdown d-flex float-end mx-3">
-                <button
-                  className="btn btn-outline-light btn-lg rounded-pill dropdown-toggle"
-                  type="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <i className="fa fa-user mx-2"></i>
-                  {store.userSession.nombre}
-                </button>
-                <ul className="dropdown-menu">
-                  <Link to={"/perfil"}>
-                    <button className="dropdown-item float-start">
-                      Mi perfil
-                    </button>
-                  </Link>
-                  <button
-                    className="dropdown-item float-start text-danger"
-                    onClick={handleLogout}
-                  >
-                    Cerrar sesión
-                  </button>
-                </ul>
-              </div>
-            ) : null}
-          </form>
-          {/* MODO-DARK */}
-          {/* <label className="switch m-2">
-            <input type="checkbox" ></input>
-            <span className="check"></span> 
-          </label> */}
-        </div>
-      </div>
-    </nav>
-  );
+                    <img src="https://i.imgur.com/tkKWUot.png" className="logo-pagina"/>
+                </Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item fs-6">
+                            <Link to="/listas"
+                                style={
+                                    {textDecoration: "none"}
+                            }>
+                                <span className="text-white m-2">Lista de mascotas</span>
+                            </Link>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        {
+                        store.auth === false ? (
+                            <Link to="/login">
+                                <button type="button" className="btn btn-outline-light mx-3 rounded-pill btn-lg">
+                                    <i className="fa fa-sign-in-alt mx-2"></i>
+                                    Iniciar sesión
+                                </button>
+                            </Link>
+                        ) : null
+                    }
+                        {
+                        store.auth === false ? (
+                            <Link to="/register">
+                                <button type="button" className="btn btn-light rounded-pill btn-lg">
+                                    <i className="fa fa-user-plus mx-2"></i>
+                                    Crear cuenta
+                                </button>
+                            </Link>
+                        ) : null
+                    }
+                        {
+                        store.auth === true ? (
+                            <div className="dropdown d-flex float-end">
+                                <button className="btn btn-light rounded-pill btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className="fa fa-plus-circle mx-2"></i>
+                                    Publicar
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <Link to={"/formencontrado"}>
+                                        <button className="dropdown-item float-start">
+                                            Mascota perdida
+                                        </button>
+                                    </Link>
+                                    <Link to={"/formadoptar"}>
+                                        <button className="dropdown-item float-start">
+                                            Mascota para adoptar
+                                        </button>
+                                    </Link>
+                                </ul>
+                            </div>
+                        ) : null
+                    }
+
+                        {
+                        store.auth === true ? (
+                            <div className="dropdown d-flex float-end mx-3">
+                                <button className="btn btn-outline-light btn-lg rounded-pill dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className="fa fa-user mx-2"></i>
+                                    {
+                                    store.userSession.nombre
+                                } </button>
+                                <ul className="dropdown-menu">
+                                    <Link to={"/perfil"}>
+                                        <button className="dropdown-item float-start">
+                                            Mi perfil
+                                        </button>
+                                    </Link>
+                                    <button className="dropdown-item float-start text-danger"
+                                        onClick={handleLogout}>
+                                        Cerrar sesión
+                                    </button>
+                                </ul>
+                            </div>
+                        ) : null
+                    } </form>
+                </div>
+            </div>
+        </nav>
+    );
 };
--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -1,4 +1,8 @@
-"../component/maps.jsx";
+import React, {useContext, useEffect} from "react";
+import {Context} from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+import {Map} from "../component/maps.jsx";
 
 export const Home = () => {
     const {store, actions} = useContext(Context);
@@ -22,8 +26,8 @@ export const Home = () => {
                     <h2>FindPets</h2>
                     <p>
                         FindPets es una aplicación web uruguaya que promueve la adopción
-                                    responsable y asiste a la comunidad en la noble tarea de volver a
-                                    reunir a las mascotas perdidas con sus familias.
+                                                            responsable y asiste a la comunidad en la noble tarea de volver a
+                                                            reunir a las mascotas perdidas con sus familias.
                     </p>
                 </div>
                 <img src="https://i.ibb.co/nCrFSbM/In-Shot-20230222-111107834.png" className="perro_gato"/>
@@ -48,8 +52,8 @@ export const Home = () => {
                             <h3>Nuestra misión</h3>
                             <p>
                                 Nuestra misión es promover la adopción responsable y asistir a
-                                                                                la comunidad en la noble tarea de volver a reunir a las mascotas
-                                                                                perdidas con sus familias.
+                                                                                                                la comunidad en la noble tarea de volver a reunir a las mascotas
+                                                                                                                perdidas con sus familias.
                             </p>
                         </div>
                     </div>
@@ -66,9 +70,9 @@ export const Home = () => {
                             <h3>¿Por qué FindPets?</h3>
                             <p>
                                 FindPets brinda los medios necesarios para que personas
-                                                                                solidarias de todo el país que encuentran mascotas perdidas
-                                                                                cuenten con un espacio para publicarlas, con el fin de que
-                                                                                vuelvan a reencontrarse con sus familias.
+                                                                                                                solidarias de todo el país que encuentran mascotas perdidas
+                                                                                                                cuenten con un espacio para publicarlas, con el fin de que
+                                                                                                                vuelvan a reencontrarse con sus familias.
                             </p>
                         </div>
                     </div>
@@ -85,9 +89,9 @@ export const Home = () => {
                             <h3>¡Es gratis!</h3>
                             <p>
                                 FindPets fue pensada y creada sin fines de lucro, sin embargo,
-                                                                                aceptamos donaciones como forma de agradecimiento para que la
-                                                                                página siga funcionando y brindando ayuda a todas las familias
-                                                                                del país.
+                                                                                                                aceptamos donaciones como forma de agradecimiento para que la
+                                                                                                                página siga funcionando y brindando ayuda a todas las familias
+                                                                                                                del país.
                             </p>
                         </div>
                     </div>
